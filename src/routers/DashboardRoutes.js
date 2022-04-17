4214609c40f1a4188284276bd3a943b1568f00eb
Fernando Schwalb <83603017+fschwalb@users.noreply.github.com>
2022-04-16 23:18:33 -0300

    heroes.js Added

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -12,15 +12,17 @@ export const DashboardRoutes = () => {
         <>
             <Navbar />
 
-            <Routes>
-                <Route path="marvel" element={<MarvelScreen />} />
-                <Route path="dc" element={<DcScreen />} />
+            <div className="container">
+                <Routes>
+                    <Route path="marvel" element={<MarvelScreen />} />
+                    <Route path="dc" element={<DcScreen />} />
 
-                <Route path="search" element={<SearchScreen />} />
-                <Route path="hero" element={<HeroScreen />} />
+                    <Route path="search" element={<SearchScreen />} />
+                    <Route path="hero" element={<HeroScreen />} />
 
-                <Route path="/" element={<MarvelScreen />} />
-            </Routes>
+                    <Route path="/" element={<MarvelScreen />} />
+                </Routes>
+            </div>
         </>
     );
 };
