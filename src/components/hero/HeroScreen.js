2c810add93a7f2186eab9ce4335b11eaa9a599d5
Fernando Schwalb <83603017+fschwalb@users.noreply.github.com>
2022-04-17 22:53:27 -0300

    HeroScreen.js Styles

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,24 +1,64 @@
-import { useParams, Navigate } from "react-router-dom";
+import { useParams, Navigate, useNavigate } from "react-router-dom";
 import { getHeroById } from "../../selectors/getHeroesById";
 
 
 export const HeroScreen = () => {
 
     const { heroeId } = useParams();
+    const navigate = useNavigate();
 
     const hero = getHeroById( heroeId );
+
+
+    const handleReturn = () => {
+
+        navigate( -1 );
+
+    };
 
     if ( !hero ) {
         return <Navigate to='/' />
     };
 
-    return (
-        <div>
-            <h1>HeroScreen</h1>
+    const {
+        id,
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+    } = hero;
 
-            <p>
-                { hero.superhero }
-            </p>
+    const imagePath = `/assets/${ id }.jpg`;
+
+    return (
+        <div className="row mt-5">
+            <div className="col-4">
+                <img 
+                    src={ imagePath } 
+                    alt={ hero.superhero } 
+                    className="img-thumbnail" 
+                />
+            </div>
+
+            <div className="col-8">
+                <h3>{ superhero }</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><b>Alter ego:</b> { alter_ego }</li>
+                    <li className="list-group-item"><b>Publisher:</b> { publisher }</li>
+                    <li className="list-group-item"><b>First Appearance:</b> { first_appearance }</li>
+                </ul>
+
+                <h5 className="mt-3">Characters</h5>
+                <p>{ characters }</p>
+
+                <button
+                    className="btn btn-outline-info"
+                    onClick={ handleReturn }
+                >
+                    Regresar
+                </button>
+            </div>
         </div>
     );
 
