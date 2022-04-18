2be0b21c0477c654d023054ba2410554e4cfbcb8
Fernando Schwalb <83603017+fschwalb@users.noreply.github.com>
2022-04-17 23:14:56 -0300

    HeroList.js and HeroScreen.js Animated.css Added

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -37,11 +37,11 @@ export const HeroScreen = () => {
                 <img 
                     src={ imagePath } 
                     alt={ hero.superhero } 
-                    className="img-thumbnail" 
+                    className="img-thumbnail animate__animated animate__bounceInLeft" 
                 />
             </div>
 
-            <div className="col-8">
+            <div className="col-8 animate__animated animate__fadeIn">
                 <h3>{ superhero }</h3>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item"><b>Alter ego:</b> { alter_ego }</li>
