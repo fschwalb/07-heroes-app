43db5c1ce75315374af4420ff87461b81e7b3db6
Fernando Schwalb <83603017+fschwalb@users.noreply.github.com>
2022-04-17 22:35:19 -0300

    getHeroesById Added

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,7 +1,25 @@
+import { useParams, Navigate } from "react-router-dom";
+import { getHeroById } from "../../selectors/getHeroesById";
 
 
 export const HeroScreen = () => {
+
+    const { heroeId } = useParams();
+
+    const hero = getHeroById( heroeId );
+
+    if ( !hero ) {
+        return <Navigate to='/' />
+    };
+
     return (
-        <h1>HeroScreen</h1>
+        <div>
+            <h1>HeroScreen</h1>
+
+            <p>
+                { hero.superhero }
+            </p>
+        </div>
     );
+
 };
